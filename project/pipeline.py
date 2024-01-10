292f265c797299f161fa122a0f47639f6863a11e
5aad <saad_anwer@live.com>
2024-01-10 16:15:32 +0100

    project submit 6

--- project/pipeline.py
+++ project/pipeline.py
@@ -2,21 +2,21 @@ import pandas as pd
 from sqlalchemy import create_engine
 from pathlib import Path
 
-def FetchData(file):
-    df = pd.read_csv(file, delimiter=";")
+def FetchData(file, deli):
+    df = pd.read_csv(file, delimiter=deli)
     return df
 
 def TransformAirQuality(file,column):
-    extract_data = FetchData(file)
+    extract_data = FetchData(file, ";")
     df = pd.DataFrame(extract_data)
-    air_df = df[df['State / Measuring network'] == 'Bavaria'].sort_values(by='Station code', ascending=True).reset_index(drop=True)
+    air_df = df[df['State / Measuring network'].isin(['Bavaria', 'Brandenburg'])].sort_values(by='Station code', ascending=True).reset_index(drop=True)
     if column:
         air_df = air_df.drop(columns='Number of daily mean values above 50 µg/m³')
         air_df = air_df.rename(columns={'Annual mean value in µg/m³': 'Annual P10 mean value in µg/m³'})
     return air_df
 
 def TransformHospital(file, columns):
-    extract_data = FetchData(file)
+    extract_data = FetchData(file,",")
     df = pd.DataFrame(extract_data)
     df['TB-Diagnose'] = df['TB-Diagnose'].replace('-', 0)
     hos_df = df.rename(columns=columns)
@@ -34,7 +34,7 @@ def Load(df, table):
 def main():
     airquality_p2_file = "https://www.umweltbundesamt.de/api/air_data/v3/annualbalances/csv?component=9&year=2022&lang=en"
     airquality_p10_file = "https://www.umweltbundesamt.de/api/air_data/v3/annualbalances/csv?component=1&year=2022&lang=en"
-    file_id = '1_foIecAE8BjeWrT_frgy_FZg_Yz6efsm'
+    file_id = '1Nmqbq2YQOmVe0ncfwAHeOQDJRXQZVhlB'
     hospital_tb_file = f'https://drive.google.com/uc?id={file_id}'
     columns = {
     "Jahr" : "Year",
